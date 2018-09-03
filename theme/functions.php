657e29578351eadff3baf9604a3f19455894e3a9
kazuki-nisshin <nisshin@dtdsh.com>
2018-09-03 16:15:44 +0900

    現段階で一時保存

--- theme/functions.php
+++ theme/functions.php
@@ -58,13 +58,13 @@ if (! function_exists( 'theme_setup' )) {
     {
 
         // Let wp know we want to use html5 for content
-        add_theme_support( 'html5', array(
-            'comment-list',
-            'comment-form',
-            'search-form',
-            'gallery',
-            'caption'
-        ) );
+        // add_theme_support( 'html5', array(
+        //     'comment-list',
+        //     'comment-form',
+        //     'search-form',
+        //     'gallery',
+        //     'caption'
+        // ) );
 
 
         // Let wp know we want to use post thumbnails
@@ -186,7 +186,7 @@ if (! function_exists( 'theme_scripts' )) {
     {
         $theme_dir = get_stylesheet_directory_uri();
         wp_deregister_script( 'jquery' );
-        wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), null, true );
+        wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), false, true );
         wp_enqueue_script( 'main', "$theme_dir/assets/js/main.js", array( 'jquery' ), DTDSH_THEME_VERSION, true );
     }
 }
